ad9a412ad36725c1aa20ac8fd1481db91931c913
Ivan Shanin <ivanxshanin@gmail.com>
2024-06-27 17:57:16 +0300

    connect to MongoDB through mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const { PORT = 3000, BASE_PATH } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/authdb');
 
 app.listen(PORT, () => {
   console.log('Ссылка на сервер:');
