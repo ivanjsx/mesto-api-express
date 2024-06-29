cd920bc7dba51b69199e9e65485bd57e2aa7ff98
Ivan Shanin <ivanxshanin@gmail.com>
2024-06-29 17:38:06 +0300

    fix relative import paths

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,13 +2,13 @@
 import { model, Schema } from "mongoose";
 
 // interfaces
-import UserInterface from "interfaces/user";
+import UserInterface from "../interfaces/user";
 
 // validators
-import urlValidator from "validators/url";
+import urlValidator from "../validators/url";
 
 // constants
-import { MAX_ABOUT_LENGTH, MAX_NAME_LENGTH, MIN_ABOUT_LENGTH, MIN_NAME_LENGTH } from "utils/constants";
+import { MAX_ABOUT_LENGTH, MAX_NAME_LENGTH, MIN_ABOUT_LENGTH, MIN_NAME_LENGTH } from "../utils/constants";
 
 
 
