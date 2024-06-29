f78450403ee8ccaad26d76de1b190c5b2e497e98
Ivan Shanin <ivanxshanin@gmail.com>
2024-06-29 14:35:43 +0300

    configure user model

---
+++ src/models/user.ts
@@ -0,0 +1,40 @@
+// libraries
+import { model, Schema } from "mongoose";
+
+// validators
+import urlValidator from "../validators/url";
+
+// constants
+import { MAX_ABOUT_LENGTH, MAX_NAME_LENGTH, MIN_ABOUT_LENGTH, MIN_NAME_LENGTH } from "utils/constants";
+
+
+
+interface UserInterface {
+  name: string;
+  about: string;
+  avatar: string;
+};
+
+const UserSchema = new Schema<UserInterface>({
+  name: {
+    type: String,
+    required: true,
+    minlength: MIN_NAME_LENGTH,
+    maxlength: MAX_NAME_LENGTH
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: MIN_ABOUT_LENGTH,
+    maxlength: MAX_ABOUT_LENGTH
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: urlValidator
+  }
+});
+
+const UserModel = model<UserInterface>("user", UserSchema);
+
+export default UserModel;
