fbd67db4f77c90e4287c71034c35e4d78134308b
Ivan Shanin <ivanxshanin@gmail.com>
2024-06-29 17:39:57 +0300

    link users routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
+// libraries
 import express from "express";
 import mongoose from "mongoose";
+
+// routes
+import usersRoutes from "./routes/users";
 
 const { PORT = 3000, BASE_PATH } = process.env;
 
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/mestodb");
+
+app.use("/users", usersRoutes);
 
 app.listen(PORT, () => {
   console.log("Ссылка на сервер:");
